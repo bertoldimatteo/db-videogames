5f5c0b0a533fc90b43d5acd0ffbea54280dae9e6
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-09-28 15:40:32 +0200

    completato fino a milestone 8

--- SQLQueryVideogame.sql
+++ SQLQueryVideogame.sql
@@ -1,3 +1,35 @@
-﻿SELECT *
-FROM software_houses
-WHERE country = 'United States';
+﻿--SELECT *
+--FROM software_houses
+--WHERE country = 'United States';
+
+--SELECT *
+--FROM players
+--WHERE city = 'Rogahnland';
+
+--SELECT *
+--FROM players
+--WHERE name LIKE '%a';
+
+--SELECT *
+--FROM reviews
+--WHERE player_id = '800';
+
+--SELECT *
+--FROM tournaments
+--WHERE year = '2015';
+
+--SELECT *
+--FROM awards
+--WHERE description LIKE '%facere%'
+
+--SELECT DISTINCT videogame_id
+--FROM category_videogame
+--WHERE category_id = '2' OR category_id = '6' 
+
+--SELECT *
+--FROM reviews
+--WHERE rating BETWEEN 2 and 4 
+
+--SELECT *
+--FROM videogames
+--WHERE release_date LIKE '%2020%'
